c8e9e30abadf402465b80698ecf1e0992a4f623c
Godwin Adakonye John <gj09042003@gmail.com>
2025-07-27 16:43:09 +0100

    Added comment Type to the sanity schema

---
+++ godwinsblog/sanity/schemaTypes/commentTypes.ts
@@ -0,0 +1,36 @@
+import { defineField, defineType } from "sanity";
+
+export const comment = defineType({
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    defineField({
+        name: "Approved",
+        title: "approved",
+        type: "boolean",
+        description: "Comments wont show on the site until approved",
+    }),
+    defineField({
+      name: "email",
+      title: "Email",
+      type: "string",
+    }),
+    defineField({
+      name: "comment",
+      title: "Comment",
+      type: "text",
+    }),
+    defineField({
+      name: "post",
+      title: "Post",
+      type: "reference",
+      to: [{ type: "post" }],
+    }),
+  ],
+});
