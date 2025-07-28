13bbedfc9fb1d294433da343cbff1e528168e0b5
Godwin Adakonye John <gj09042003@gmail.com>
2025-07-28 05:22:42 +0100

    Fixed the google auth bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images:{
-    domains: ["cdn.sanity.io"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**", // Matches Google profile image paths (e.g., /a/ACg8ocIl...)
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/images/**", // Matches Sanity image paths
+      },
+    ],
   },
 };
